9078b52a5ed82d475ab5f4dc76e754601b5161af
Charlemagne4 <mohdingo4@gmail.com>
2024-10-28 16:50:09 +0100

    fix index page bug when no search query is defined and higher image quality are now available

--- controllers/games.js
+++ controllers/games.js
@@ -1,56 +1,62 @@
-const axios = require('axios')
+const axios = require('axios');
+
 // Define your headers
 const headers = {
     'Client-ID': process.env.ClientID,           // Replace with your actual Client ID
-    'Authorization': process.env.Authorization, // Replace with your actual token
-    'Content-Type': 'text/plain'       // Specify JSON content type
+    'Authorization': process.env.Authorization,  // Replace with your actual token
+    'Content-Type': 'text/plain'                 // Specify JSON content type
 };
+
 const url = 'https://api.igdb.com/v4/games';
 
-
-module.exports.index = async (req, res) => {
-    // Define the custom request body
-    const name = req.query.game.name;
-    const data = `
-    search "${name}"; fields name, genres.name, cover.url, rating;
-    limit 10;
-`;
+module.exports.index = async (req, res, next) => {
     try {
+        // Validate query parameters
+        let name = req.query.game?.name;
+        let data;
+        if (!name) {
+            data = `
+                fields name, cover.url, cover.image_id, rating;
+                where rating > 95;
+                limit 10;
+            `;
+        } else {
+
+            data = `
+                search "${name}"; fields name, cover.image_id, rating;
+                limit 10;
+            `;
+        }
         // Make the Axios request
         const response = await axios.post(url, data, { headers });
-        //console.log(response);
 
         // Extract the games data from the response
         const games = response.data;
 
         // Render the view with the games data
-        res.render('games/index', { games });
-    } catch (error) {
-        console.error('Error fetching data from IGDB:', error.response ? error.response.data : error.message);
-        res.status(500).render('error', { error: error.message }); // Render an error view
+        res.render('games/index', { games, imageSize: "logo_med" });
+    } catch (err) {
+        console.error('Error fetching data from IGDB:', err.response ? err.response.data : err.message);
+        next(err);
     }
 }
 
-module.exports.show = async (req, res) => {
-    //console.log(req.params);
-
-    const gameId = req.params.id
+module.exports.show = async (req, res, next) => {
+    const gameId = req.params.id;
     const data = `
-    fields name, genres.name, cover.url, rating; where id = ${gameId};
-`;
+        fields name, genres.name, cover.image_id, rating; where id = ${gameId};
+    `;
     try {
         // Make the Axios request
         const response = await axios.post(url, data, { headers });
-        //console.log(response);
 
-        // Extract the games data from the response
+        // Extract the game data from the response
         const game = response.data;
-        console.log(game);
 
-        // Render the view with the games data
-        res.render('games/show', { game });
-    } catch (error) {
-        console.error('Error fetching data from IGDB:', error.response ? error.response.data : error.message);
-        res.status(500).render('error', { error: error.message }); // Render an error view
+        // Render the view with the game data
+        res.render('games/show', { game, imageSize: "720p" });
+    } catch (err) {
+        console.error('Error fetching data from IGDB:', err.response ? err.response.data : err.message);
+        next(err);
     }
 }

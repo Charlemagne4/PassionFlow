1a7fba5a20b98964580a6625dbe0a86bf138c16d
Charlemagne4 <mohdingo4@gmail.com>
2024-10-28 20:57:44 +0100

    fixed catchAsync and now it provides better resilience against errors in both asynchronous and synchronous function

--- Utility/catchAsync.js
+++ Utility/catchAsync.js
@@ -1,5 +1,11 @@
 module.exports = func => {
     return (req, res, next) => {
-        func(req, res, next).catch(e => next(e));
-    }
-}
+        // Wrap the function call in a try-catch block for synchronous errors
+        try {
+            // Call the provided function and handle async errors
+            Promise.resolve(func(req, res, next)).catch(next);
+        } catch (err) {
+            next(err); // Handle synchronous errors
+        }
+    };
+};
